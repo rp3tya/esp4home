4fa211fbc908b4b79bb17adc415809c5a2e61b26
peter <peter@aw.ro>
2019-11-11 18:26:52 +0200

    minimal version of RF temperature sensor

---
+++ esphome/hckk04.h
@@ -0,0 +1,67 @@
+#include "esphome.h"
+
+const uint8_t RX_PIN = D6;
+volatile uint64 pack = 0;
+
+static inline unsigned int diff(int A, int B) {
+  return abs(A - B);
+}
+ICACHE_RAM_ATTR void handler() {
+  static uint64 code = 0;
+  static uint8 cnt = 0;
+  static unsigned long lastTime = 0;
+  //
+  const unsigned long time = micros();
+  const unsigned long duration = time - lastTime;
+  //
+  if (duration > 500 && duration < 700) {
+    if (digitalRead(RX_PIN) == LOW) {
+      // pulse
+    }
+  } else if (duration > 700 && duration < 900) {
+    if (digitalRead(RX_PIN) == HIGH) {
+      code <<= 1;
+      cnt++;
+      code |= 0;
+    }
+  } else if (duration > 1500 && duration < 1900) {
+    if (digitalRead(RX_PIN) == HIGH) {
+      code <<= 1;
+      cnt++;
+      code |= 1;
+    }
+  } else if (duration > 3500) {
+    if (cnt == 36) pack = code;
+    code = 0;
+    cnt = 0;
+  } else {
+    code = 0;
+    cnt = 0;
+  }
+  //
+  lastTime = time;
+}
+
+/**
+ * Sensor
+ */
+class HCKK04Sensor : public Component, public Sensor {
+ public:
+  void setup() override {
+    pinMode(RX_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(RX_PIN), handler, CHANGE);
+  }
+  void loop() override {
+    if (pack > 0) {
+      publish_state((pack >> 12 & 0xFFF)/10.0);
+//    uint32 id = pack >> 28 & 0xFF;
+//    uint32 fl = pack >> 24 & 0xF;
+//    uint32 tm = pack >> 12 & 0xFFF;
+//    uint32 cs = pack >> 8 & 0xF;
+//    uint32 hm = pack >> 0 & 0xFF;
+      pack = 0;
+    }
+  }
+};
+
+
